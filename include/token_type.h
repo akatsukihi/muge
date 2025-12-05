6c430e01c0f033c11975857a75c1772884cd0f74
暁 <xiaoyang.zhang@queenbeecapital.com>
2025-12-05 18:57:54 +0900

    create token type

---
+++ include/token_type.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef __TOKENTYPE_H__
+
+#define __TOKENTYPE_H__
+
+enum class TokenType{
+
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE
+
+
+
+};
+
+
+#endif
+
